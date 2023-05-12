475c5f274bb9f02fc22ae032f4ed70527b592158
Michael Gichuhi <gmikekamau@gmail.com>
2023-05-12 14:06:14 +0300

    💄 began design the about page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import IMAGES from '../../assets/images'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div 
+    className='flex bg-primary-blue rounded-xl px-2 mx-2 my-2  shadow-2xl transition-all '>
+      <h1 
+      className='text-primary-light-green text-5xl mt-28  px-5 mx-1 '>Are you tired of cleaning then it's time to call us</h1>
+        <div 
+        className='px-4 mx-3 mt-3'>
+          <img src={IMAGES.img1}  alt="" 
+          className='max-w-lg rounded-3xl h-96 mb-3 transform transition-all hover:scale-95 ' />
+        </div>
+    </div>
+    </>
   )
 }
