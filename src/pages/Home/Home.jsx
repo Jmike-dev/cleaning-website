bfb370ea3874076c72f24a3010bff5f8bdb603c1
Michael Gichuhi <gmikekamau@gmail.com>
2023-05-16 17:57:51 +0300

    💄 designed the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import IMAGES from '../../assets/images'
+// import About from './components/About'
+
 
 export default function Home() {
   return (
-    <>
     <div 
-    className='flex bg-primary-blue rounded-xl px-2 mx-2 my-2  shadow-2xl transition-all '>
-      <h1 
-      className='text-primary-light-green text-5xl mt-28  px-5 mx-1 '>Are you tired of cleaning then it's time to call us</h1>
+    className='bg-primary-grey flex-bottom pt-4 rounded-xl mx-3  mt-5 pb-4'>
         <div 
-        className='px-4 mx-3 mt-3'>
-          <img src={IMAGES.img1}  alt="" 
-          className='max-w-lg rounded-3xl h-96 mb-3 transform transition-all hover:scale-95 ' />
+        className='flex bg-primary-blue rounded-xl px-2 mx-2 my-2  shadow-2xl transition-all '>
+          <div className='flex relative'>
+            <h1 
+            className='text-primary-grey hover:text-primary-dark-grey text-5xl mt-28   mx-44  mb-44'>
+              Are you tired of cleaning then it's time to call us</h1>
+              <button className='bg-primary-green py-2 px-4   hover:bg-primary-light-green   rounded-full absolute bottom-12 left-40'>book now</button>
+          </div>
+            <div 
+            className='px-4 mx-3 mt-3'>
+              <img src={IMAGES.img1}  alt="" 
+              className='max-w-lg  flex rounded-3xl h-96 mb-3 transform transition-all hover:scale-95 ' />
+            </div>
         </div>
+        {/* <About /> */}
     </div>
-    </>
   )
 }
---
+++ src/pages/Home/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div 
+    className='bg-primary-light-green flex-bottom rounded-3xl px-3 mx-20 -mt-3'>
+        <h1 
+        className='text-2xl text-primary-blue'>
+            What do we do
+        </h1>
+            <h2 
+            className='text'>
+                Our job is to clean your home as well as you do.
+            </h2>
+    </div>
+  )
+}
