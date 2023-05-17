96d001b19e3396e5828ac5a301adcaaf03828b1b
Michael Gichuhi <gmikekamau@gmail.com>
2023-05-17 21:22:56 +0300

    changed the colour pallete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,14 +6,19 @@ import About from './components/About'
 export default function Home() {
   return (
     <>
-    <div  className='bg-primary-dark-grey  rounded-b-lg  md:w-full sm:w-auto py-2 '>
+    {/* home parent div  container */}
+    <div  className='bg-primary-black  rounded-b-lg  md:w-full sm:w-auto py-2 '>
+      {/* home child div container */}
        <div className='w-full relative '>
+        {/* image div container */}
           <div className=' flex justify-end py-2 mx-3 ' >
             <img className='h-80 w-auto rounded-3xl ' src={IMAGES.img1} alt=""  />
           </div>
+          {/* h1  div container */}
           <div className='relative left-1 md:absolute md:left-5 md:top-5 md:w-1/3 bottom-2'>
-            <h1 className='text-xl text-primary-light-green hover:text-primary-green md:text-5xl '>Are you tired of cleaning then it's time to call us</h1>
+            <h1 className='text-xl text-primary-blue hover:text-primary-green md:text-5xl '>Are you tired of cleaning then it's time to call us</h1>
           </div>
+          {/* button div container */}
           <div className='relative py-1'>
               <a href="/" class=" absolute  bottom-1 left-64  rounded-full px-4 py-2 text-white bg-primary-green transition
                ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100 hover:text-primary-blue hover:bg-primary-light-green md:bottom-28 md:left-78 md:text-3xl">Book Now</a>
--- src/pages/Home/components/About.jsx
+++ src/pages/Home/components/About.jsx
@@ -2,15 +2,29 @@ import React from 'react'
 
 export default function About() {
   return (
-  <div className='md:w-full'>
-    <div className='bg-primary-light-green  rounded-3xl px-3 mx-10 mt-3'>
-        <h1  className='text-2xl text-primary-blue'>
-            What do we do
-        </h1>
-            <h2 className=''>
-                Our job is to clean your home as well as you do.
-            </h2>
-    </div>
-  </div>  
+   <>
+  {/* about parent div container */}
+  <div className=" md:w-full bg-primary-purple my-2 rounded-xl">
+    {/*  about child div container*/}
+    <div>
+      {/* h1 div container */}
+      <div classname="px-16 md:px-64 " >
+      <br />
+       <hr className="w-3/5" />
+       <h1 className="w-1/2 mt-6 text-5xl font-headline tracking-tight font-extrabold text-primary-black leading-snug">
+         What do we do <br />
+         <span className="text-primary-blue w-1/2">Our job is to clean your home as well as you do.</span>
+       </h1>
+      </div>
+       {/* p div container */}
+      <div>
+       <p className="w-3/5 mt-2 text-primary-black text-lg" >
+        If your job is very busy and you are tired of cleaning your home then "name of the business" is for you.</p>
+      </div>
+
+       </div>
+  </div>
+
+  </>
   )
 }
