ab20eb3be7554d6fbe7d6db7c4d646efcf0a24ae
Michael Gichuhi <gmikekamau@gmail.com>
2023-05-18 12:43:00 +0300

    📱 better mobile design for the home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,12 +15,12 @@ export default function Home() {
             <img className='h-64 w-auto rounded-xl md:h-80 ' src={IMAGES.img1} alt=""  />
           </div>
           {/* h1  div container */}
-          <div className='relative left-1 md:absolute md:left-5 md:top-5 md:w-1/3 bottom-2'>
-            <h1 className='text-xl text-primary-blue hover:text-primary-green md:text-5xl '>Are you tired of cleaning then it's time to call us</h1>
+          <div className='relative left-6 md:absolute md:left-5 md:top-5 md:w-1/3 bottom-2 mx-24 my-10'>
+            <h1 className='text-2xl text-primary-blue hover:text-primary-green md:text-5xl '>Are you tired of cleaning then it's time to call us</h1>
           </div>
           {/* button div container */}
           <div className='relative py-1'>
-              <a href="/" class=" absolute  bottom-1 left-32  rounded-full px-4 py-2 text-white bg-primary-green transition
+              <a href="/" class=" absolute  bottom-1 left-36 rounded-lg md:rounded-full px-4 py-2 text-white bg-primary-green transition
                ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100 hover:text-primary-blue hover:bg-primary-light-green md:bottom-28 md:left-78 md:text-3xl">Book Now</a>
           </div>
       </div>
