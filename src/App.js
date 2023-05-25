7a781a1ba25268445615216820cb68fab37be07a
Michael Gichuhi <gmikekamau@gmail.com>
2023-05-25 15:09:46 +0300

    📱 added a carousel and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
 import FooterSection from './layout/Footer/FooterSection';
 import HeaderSection from './layout/Header/HeaderSection';
-import { BrowserRouter } from 'react-router-dom'
 
 
 function App() {
   return (
    <>
-   <BrowserRouter>
     <HeaderSection/>
    <FooterSection/>
-   </BrowserRouter>
    </>
   );
 }
--- src/assets/images/images.jsx
+++ src/assets/images/images.jsx
@@ -1,7 +1,9 @@
 const IMAGES = {
     img1: require('./cleaning.jpg'),
     img2: require('./housecleaning.jpg'),
-    img3: require('./officecleaning.jpg')
+    img3: require('./officecleaning.jpg'),
+    slide1: require('./carosel.jpg'),
+    slide2: require('./carosel1.jpg')
 }
 
 export default IMAGES
